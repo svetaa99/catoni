1dd8fddac2cb0113f9e226f5ecb34a512466735b
x <filip.kresa@gmail.com>
2022-06-06 19:55:22 +0200

    Add timer and TooManyOffersEvent.

--- catoni/src/main/java/com/catoni/services/TradeService.java
+++ catoni/src/main/java/com/catoni/services/TradeService.java
@@ -4,6 +4,7 @@ import com.catoni.models.GlobalState;
 import com.catoni.models.InputState;
 import com.catoni.models.Trade;
 import com.catoni.models.TradeOffer;
+import org.kie.api.internal.utils.KieService;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +13,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class TradeService {
     private final KieContainer kieContainer;
+    private final KieSession kieSession;
 
     private int tradeCnt = 0;
 
     @Autowired
     public TradeService(KieContainer kieContainer){
         this.kieContainer = kieContainer;
+        this.kieSession = kieContainer.newKieSession();
     }
 
     public TradeOffer getTradeOffer(InputState inputState){
         TradeOffer offer = new TradeOffer();
-
-        KieSession kieSession = kieContainer.newKieSession();
+        System.out.println(offer);
         kieSession.insert(inputState);
         kieSession.insert(offer);
         kieSession.insert(GlobalState.getInstance());
         kieSession.fireAllRules();
-        kieSession.dispose();
+//        kieSession.dispose();
 
         return offer;
     }
